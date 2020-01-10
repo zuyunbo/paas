d24cc2fd8aa9cbbe37af7320ad617dfe09340ef9
zuyunbo <12345678>
2020-01-11 00:36:25 +0800

    增加用户实体

---
+++ api-modle/src/main/java/com/cloud/modle/apimodle/ApiModleApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.modle.apimodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiModleApplication.class, args);
+    }
+
+}
